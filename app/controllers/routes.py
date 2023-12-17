df9913d692a9147fe194f87e858160f9d56e3916
Anderson Albuquerque <118145194+ALBUKERKTIPS@users.noreply.github.com>
2023-12-17 18:03:44 +0000

    Starting HTML
    starting some routes and html

--- app/controllers/routes.py
+++ app/controllers/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+
+@app.route('/create-account', methods=['GET', 'POST'])
+def create():
+    return render_template("create-account.html")
